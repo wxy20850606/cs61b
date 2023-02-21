13880a73c1427961a799fb5bd8c3e5059b61c9ef
wangxiaoyan <wxy20850606@gmail.com>
2023-02-21 14:58:10 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,11 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+    /** check if the given year is a leap year
+    */
+    public static boolean isLeapYear(int year){
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
