99c55a83e7e79ba4beb1f6b9aec3920b44b5e8af
wangxiaoyan <wxy20850606@gmail.com>
2023-03-06 15:23:47 +0800

    create LinkedListDeque.java

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,39 @@
+public class LinkedListDeque{
+
+/*variable */
+
+
+/*contructor */
+
+public void addFirst(T item){
+return 1;
+}
+
+public void addLast(T item){
+return 1;
+}
+
+public boolean isEmpty(){
+return 1;
+}
+
+public int size(){
+return 1;
+}
+
+public void printDeque(){
+return 1;
+}
+
+public T removeFirst(){
+return 1;
+}
+
+public T removeLast(){
+return 1;
+}
+
+public T get(int index){
+    return 1;
+}
+}
