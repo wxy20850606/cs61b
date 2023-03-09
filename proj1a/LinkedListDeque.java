0d615a661efcaea9f8a4f72aca36657d3a2fc65c
wangxiaoyan <wxy20850606@gmail.com>
2023-03-09 11:05:05 +0800

    add addLast function

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -5,44 +5,49 @@ public class LinkedListDeque<T>{
     private class StuffNode{
         public T item;
         public StuffNode next;
+        public StuffNode prev;
 
-        public StuffNode(T i, StuffNode n) {
+        public StuffNode(T i) {
+            item = i;
+        }
+        public StuffNode(T i,StuffNode n,StuffNode p) {
             item = i;
             next = n;
+            prev = p;
         }
     }
 
     /*instance variable */
     private StuffNode sentinel;
+    private StuffNode node;
     private int size;
 
-/*constructor/create a empty list */
-public LinkedListDeque(){
-    sentinel = new StuffNode;
-    size = 0;
-}
+/*create an empty list */
 public LinkedListDeque(T item){
-    sentinel = new StuffNode(item,null);
-
-    sentinel.next = new StuffNode(item,sentinel);
-    size = 1;
+    sentinel = new StuffNode(item);
+    sentinel.prev = sentinel;
+    sentinel.next = sentinel;
+    size = 0;
 }
 
 public void addFirst(T item){
 
-    sentinel.next = new StuffNode(item,sentinel.next);
+    sentinel.next  = new StuffNode(item,sentinel.next,sentinel);
+    //node = sentinel.next;
+    //sentinel.prev = sentinel.prev.prev.next;
+    //sentinel.prev.prev = sentinel.next;
+    sentinel.next.next.prev = sentinel.next;
+
+
+
 
     size = size + 1;
 }
 
 public void addLast(T item){
-    StuffNode p = sentinel.next;
-    for(int i = 1;i < size; i += 1){
-        p = p.next;
-    }
-    p.next = new StuffNode(item,sentinel);
+    sentinel.prev.next= new StuffNode(item,sentinel,sentinel.prev);
+    sentinel.prev = sentinel.prev.next;
     size += 1;
-
 }
 
 public boolean isEmpty(){
@@ -90,9 +95,11 @@ public static void main(String[] args){
 
     /*create a list of string */
     LinkedListDeque<String> s1 = new LinkedListDeque<>("cs61b");
+    s1.addFirst("very");
+    s1.addFirst("u");
     s1.addFirst("love");
     s1.addFirst("i");
-    s1.addLast("last");
+    s1.addLast("much");
     s1.printDeque();
     System.out.println(s1.size);
 }
