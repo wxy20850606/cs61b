c90a2673621bd6fa45355f31bae9afa2bf0ccc5e
wangxiaoyan <wxy20850606@gmail.com>
2023-04-24 14:26:57 +0800

    finish the constructor

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
@@ -28,11 +28,19 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Instance Variables */
     private Collection<Node>[] buckets;
     // You should probably define some more!
+    private int initialCapacity = 16;
+    private double loadFactor = 0.75;
+    private  int size = 0;
+
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(16,0.75);
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this(initialSize,0.75);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialSize.
@@ -41,13 +49,15 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialSize initial size of backing array
      * @param maxLoad maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
-
+    public MyHashMap(int initialSize, double maxLoad) {
+        this.buckets = createTable(initialSize);
+        this.loadFactor = maxLoad;
+    }
     /**
      * Returns a new node to be placed in a hash table bucket
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key,value);
     }
 
     /**
@@ -62,6 +72,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * Most data structures in Java inherit from Collection, so we
      * can use almost any data structure as our buckets.
      *
+
      * Override this method to use different data structures as
      * the underlying bucket type
      *
@@ -69,7 +80,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new ArrayList<>();
     }
 
     /**
@@ -81,11 +92,102 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      *
      * @param tableSize the size of the table to create
      */
+
+    //update the following part from https://github.com/exuanbo/cs61b-sp21/tree/main/lab8/hashmap
     private Collection<Node>[] createTable(int tableSize) {
-        return null;
+        Collection<Node>[] table = new Collection[tableSize];
+        for(int i = 0; i < tableSize;i += 1){
+            table[i] = createBucket();
+        }
+        return table;
+
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+
+    @Override
+    public void clear(){
+        size = 0;
+        buckets = null;
+    }
+
+    @Override
+    public boolean containsKey(K key){
+        return getNode(key) != null;
+    }
+    private Node getNode(K key) {
+        int mod = modCalculator(key);
+        Collection<Node> bucket = buckets[mod];
+        for (Node node : bucket) {
+            if (node.key.equals(key)) {
+                return node;
+            }
+        }
+        return null;
+    }
+    @Override
+    public  V get(K key){
+        if(containsKey(key)){
+            Node node = getNode(key);
+            return node.value;
+        }
+        return null;
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+    private void resize(){
+        initialCapacity = initialCapacity * 2;
+        for(int i = 0; i < size; i += 1){
+            rePutNode();}
+    }
+
+    private void rePutNode(){
+        throw new UnsupportedOperationException();
+
+    }
+
+    // calculate the mode for a given key use Math.floorMod
+    private int modCalculator(K key){
+        int hashCode = key.hashCode();
+        int mod = Math.floorMod(hashCode,initialCapacity);
+        return mod;
+    }
+    @Override
+    public void put(K key, V value){
+        int mod = modCalculator(key);
+        Node node = createNode(key,value);
+        buckets[mod].add(node);
+        size += 1;
+    }
+    @Override
+    public Set<K> keySet(){
+        Set<K> set = new HashSet<>();
+        for(int i = 0; i < initialCapacity; i += 1){
+            throw new UnsupportedOperationException();
+        }
+        return set;
+    }
+    @Override
+    public V remove(K key){ throw new UnsupportedOperationException();
+    }
+    @Override
+    public V remove(K key, V value){ throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator(){
+        return keySet().iterator();
+    }
+
+    public static void main(String[] args) {
+        MyHashMap<String, String> a = new MyHashMap<>();
+        a.put("abc","d");
+    }
+
+
 
 }
